ba46de09d4ad514124d5a6f99294af5d3d8069f0
yakanechi <138551445+yakanechi@users.noreply.github.com>
2024-04-03 08:50:34 +0900

    fix

--- plugins/builds/types/models.d.ts
+++ plugins/builds/types/models.d.ts
@@ -64,6 +64,226 @@ export type PipelineModel = {
     getMetrics: () => unknown;
     chainPR: () => boolean;
 };
+
+// -----------------------------------------------------------------------------
+//      screwdriver-models.Job
+// -----------------------------------------------------------------------------
+/**
+ * Job is defined in 'screwdriver-models/lib/job.js'
+ *
+ * @description import {Job} from 'screwdriver-models/lib/job'
+ */
+export type Job = {
+    // property
+    title: undefined;
+    createTime: undefined;
+    username: undefined;
+    userProfile: undefined;
+    url: undefined;
+    id: number;
+    name: string;
+    prParentJobId: undefined;
+    permutations: Array<Permutation>;
+    description: undefined;
+    pipelineId: number;
+    state: string;
+    stateChanger: undefined;
+    stateChangeTime: undefined;
+    stateChangeMessage: undefined;
+    archived: boolean;
+    templateId: undefined;
+
+    // symbol
+    '[symbol(model)]': undefined;
+    '[symbol(table)]': undefined;
+    '[symbol(datastore)]': undefined;
+    '[symbol(scm)]': undefined;
+    '[symbol(multiBuildClusterEnabled)]': undefined;
+    '[symbol(row data)]': undefined;
+    '[symbol(dirty variable)]': undefined;
+    '[symbol(executor)]': undefined;
+    '[symbol(tokenGen)]': undefined;
+    '[symbol(apiUri)]': undefined;
+
+    // functions
+    pipeline: Promise<unknown>
+    secrets: Promise<unknown>
+    isPR: () => unknown;
+    parsePRJobName: () => unknown;
+    prNum: object;
+    getBuilds: () => unknown;
+    getRunningBuilds: () => unknown;
+    getLatestBuild: () => unknown;
+    update: () => unknown;
+    remove: () => unknown;
+    getMetrics: () => unknown;
+};
+
+// -----------------------------------------------------------------------------
+//      screwdriver-models.Job
+// -----------------------------------------------------------------------------
+/**
+ * BuildModel is defined in 'screwdriver-models/lib/build.js'
+ *
+ * @description import {BuildModel} from 'screwdriver-models/lib/build'
+ */
+export type BuildModel = {
+    // property
+    id: number;
+    environment: Array<Record<string, string>>;
+    eventId: number;
+    jobId: number;
+    parentBuildId: Array<number>,
+    parentBuilds: ParentBuilds;
+    number: number;
+    container: string;
+    cause: string;
+    sha: string;
+    subscribedConfigSha: undefined;
+    createTime: string;
+    startTime: string;
+    endTime: string;
+    parameters: undefined;
+    meta: {
+        build: {
+            buildId: string;
+            eventId: string;
+            jobId: string;
+            jobName: string;
+            pipelineId: string;
+            sha: string;
+        };
+        commit: {
+            author: {
+                avatar: string;
+                id: string;
+                name: string;
+                url: string;
+                username: string;
+            };
+            changedFiles: string;
+            committer: {
+                avatar: string;
+                id: string;
+                name: string;
+                url: string;
+                username: string;
+            };
+            message: string;
+            url: string;
+        };
+        event: {
+            creator: string;
+        };
+    };
+    status: string;
+    statusMessage: undefined;
+    stats: object;
+    templateId: undefined;
+    buildClusterName: undefined;
+
+    // symbol
+    '[symbol(model)]': undefined;
+    '[symbol(table)]': undefined;
+    '[symbol(datastore)]': undefined;
+    '[symbol(scm)]': ScmRouter;
+    '[symbol(multiBuildClusterEnabled)]': boolean;
+    '[symbol(row data)]': undefined;
+    '[symbol(dirty variable)]': undefined;
+    '[symbol(executor)]': undefined;
+    '[symbol(apiUri)]': undefined;
+    '[symbol(tokenGen)]': undefined;
+    '[symbol(uiUri)]': undefined;
+
+    // functions
+    updateCommitStatus: () => unknown;
+    getSteps: () => unknown;
+    job: Promise<unknown>
+    pipeline: Promise<unknown>
+    secrets: Promise<unknown>
+    start: () => unknown;
+    update: () => unknown;
+    remove: () => unknown;
+    stop: () => unknown;
+    stopFrozen: () => unknown;
+    unzipArtifacts: () => unknown;
+    isDone: () => unknown;
+    getMetrics: () => unknown;
+    toJsonWithSteps: () => unknown;
+};
+
+// -----------------------------------------------------------------------------
+//      screwdriver-models.EventModel
+// -----------------------------------------------------------------------------
+/**
+ * EventModel is defined in 'screwdriver-models/lib/event.js'
+ *
+ * @description import {EventModel} from 'screwdriver-models/lib/event'
+ */
+export type EventModel = {
+    // property
+    id: number;
+    parentEventId: undefined;
+    groupEventId: number;
+    causeMessage: string;
+    commit: Commit;
+    createTime: string;
+    creator: Creator;
+    meta: Meta;
+    pipelineId: number;
+    sha: string;
+    configPipelineSha: undefined;
+    startFrom: string;
+    type: string;
+    workflowGraph: WorkflowGraph;
+    pr: object;
+    prNum: undefined;
+    baseBranch: string;
+
+    // symbol
+    '[symbol(model)]': undefined;
+    '[symbol(table)]': undefined;
+    '[symbol(datastore)]': undefined;
+    '[symbol(scm)]': ScmRouter;
+    '[symbol(multiBuildClusterEnabled)]': undefined;
+    '[symbol(row data)]': undefined;
+    '[symbol(dirty variable)]': undefined;
+
+    // functions
+    getStageBuilds: () => unknown;
+    getBuilds: () => unknown;
+    getMetrics: () => unknown;
+};
+
+// -----------------------------------------------------------------------------
+//      screwdriver-models.UserModel
+// -----------------------------------------------------------------------------
+/**
+ * UserModel is defined in 'screwdriver-models/lib/user.js'
+ *
+ * @description import {UserModel} from 'screwdriver-models/lib/user'
+ */
+export type UserModel = {
+    id: number;
+    username: string;
+    token: string;
+    scmContext: string;
+    settings: Record<string, settings>;
+}
+
+// -----------------------------------------------------------------------------
+//      screwdriver-models.StageModel
+// -----------------------------------------------------------------------------
+/**
+ * StageModel is defined in 'screwdriver-models/lib/stage.js'
+ *
+ * @description import {StageModel} from 'screwdriver-models/lib/stage'
+ */
+export type StageModel = object;
+
+// -----------------------------------------------------------------------------
+//      Dependencies
+// -----------------------------------------------------------------------------
 type WorkflowGraph = {
     nodes: Array<WorkflowGraphNode>,
     edges: Array<WorkflowGraphEdges>,
@@ -84,6 +304,7 @@ type ScmRepo = {
     rootDir: string;
     private: boolean;
 }
+
 /**
  * ScmRouter is defined in 'screwdriver-scm-router/index.js'
  *
@@ -168,60 +389,6 @@ type ScmRouter = {
     getReadOnlyInfo: () => unknown;
 }
 
-// -----------------------------------------------------------------------------
-//      screwdriver-models.Job
-// -----------------------------------------------------------------------------
-/**
- * Job is defined in 'screwdriver-models/lib/job.js'
- *
- * @description import {Job} from 'screwdriver-models/lib/job'
- */
-export type Job = {
-    // property
-    title: undefined;
-    createTime: undefined;
-    username: undefined;
-    userProfile: undefined;
-    url: undefined;
-    id: number;
-    name: string;
-    prParentJobId: undefined;
-    permutations: Array<Permutation>;
-    description: undefined;
-    pipelineId: number;
-    state: string;
-    stateChanger: undefined;
-    stateChangeTime: undefined;
-    stateChangeMessage: undefined;
-    archived: boolean;
-    templateId: undefined;
-
-    // symbol
-    '[symbol(model)]': undefined;
-    '[symbol(table)]': undefined;
-    '[symbol(datastore)]': undefined;
-    '[symbol(scm)]': undefined;
-    '[symbol(multiBuildClusterEnabled)]': undefined;
-    '[symbol(row data)]': undefined;
-    '[symbol(dirty variable)]': undefined;
-    '[symbol(executor)]': undefined;
-    '[symbol(tokenGen)]': undefined;
-    '[symbol(apiUri)]': undefined;
-
-    // functions
-    pipeline: Promise<unknown>
-    secrets: Promise<unknown>
-    isPR: () => unknown;
-    parsePRJobName: () => unknown;
-    prNum: object;
-    getBuilds: () => unknown;
-    getRunningBuilds: () => unknown;
-    getLatestBuild: () => unknown;
-    update: () => unknown;
-    remove: () => unknown;
-    getMetrics: () => unknown;
-};
-
 type Permutation = {
     annotations: Record<string, number | string | boolean>;
     commands: Array<Command>
@@ -231,175 +398,18 @@ type Permutation = {
     settings: object;
     requires: Array<string>,
 }
+
 type Command = {
     name: string;
     command: string;
 }
 
-
-// -----------------------------------------------------------------------------
-//      screwdriver-models.Job
-// -----------------------------------------------------------------------------
-/**
- * BuildModel is defined in 'screwdriver-models/lib/build.js'
- *
- * @description import {BuildModel} from 'screwdriver-models/lib/build'
- */
-export type BuildModel = {
-    // property
-    id: number;
-    environment: Array<Record<string, string>>;
-    eventId: number;
-    jobId: number;
-    parentBuildId: Array<number>,
-    parentBuilds: ParentBuilds;
-    number: number;
-    container: string;
-    cause: string;
-    sha: string;
-    subscribedConfigSha: undefined;
-    createTime: string;
-    startTime: string;
-    endTime: string;
-    parameters: undefined;
-    meta: {
-        // property
-        build: {
-            // property
-            buildId: string;
-            eventId: string;
-            jobId: string;
-            jobName: string;
-            pipelineId: string;
-            sha: string;
-            // symbol
-            // functions
-        };
-        commit: {
-            // property
-            author: {
-                // property
-                avatar: string;
-                id: string;
-                name: string;
-                url: string;
-                username: string;
-                // symbol
-                // functions
-            };
-            changedFiles: string;
-            committer: {
-                // property
-                avatar: string;
-                id: string;
-                name: string;
-                url: string;
-                username: string;
-                // symbol
-                // functions
-            };
-            message: string;
-            url: string;
-            // symbol
-            // functions
-        };
-        event: {
-            // property
-            creator: string;
-            // symbol
-            // functions
-        };
-        // symbol
-        // functions
-    };
-    status: string;
-    statusMessage: undefined;
-    stats: {
-        // property
-        // symbol
-        // functions
-    };
-    templateId: undefined;
-    buildClusterName: undefined;
-
-    // symbol
-    '[symbol(model)]': undefined;
-    '[symbol(table)]': undefined;
-    '[symbol(datastore)]': undefined;
-    '[symbol(scm)]': ScmRouter;
-    '[symbol(multiBuildClusterEnabled)]': boolean;
-    '[symbol(row data)]': undefined;
-    '[symbol(dirty variable)]': undefined;
-    '[symbol(executor)]': undefined;
-    '[symbol(apiUri)]': undefined;
-    '[symbol(tokenGen)]': undefined;
-    '[symbol(uiUri)]': undefined;
-
-    // functions
-    updateCommitStatus: () => unknown;
-    getSteps: () => unknown;
-    job: Promise<unknown>
-    pipeline: Promise<unknown>
-    secrets: Promise<unknown>
-    start: () => unknown;
-    update: () => unknown;
-    remove: () => unknown;
-    stop: () => unknown;
-    stopFrozen: () => unknown;
-    unzipArtifacts: () => unknown;
-    isDone: () => unknown;
-    getMetrics: () => unknown;
-    toJsonWithSteps: () => unknown;
-};
-
 type ParentBuilds = Record<string, ParentBuild>;
+
 export type ParentBuild = {
     eventId: number;
     jobs: Record<string, number>,
 }
-
-// -----------------------------------------------------------------------------
-//      screwdriver-models.EventModel
-// -----------------------------------------------------------------------------
-/**
- * EventModel is defined in 'screwdriver-models/lib/event.js'
- *
- * @description import {EventModel} from 'screwdriver-models/lib/event'
- */
-export type EventModel = {
-    // property
-    id: number;
-    parentEventId: undefined;
-    groupEventId: number;
-    causeMessage: string;
-    commit: Commit;
-    createTime: string;
-    creator: Creator;
-    meta: Meta;
-    pipelineId: number;
-    sha: string;
-    configPipelineSha: undefined;
-    startFrom: string;
-    type: string;
-    workflowGraph: WorkflowGraph;
-    pr: object;
-    prNum: undefined;
-    baseBranch: string;
-
-    // symbol
-    '[symbol(model)]': undefined;
-    '[symbol(table)]': undefined;
-    '[symbol(datastore)]': undefined;
-    '[symbol(scm)]': ScmRouter;
-    '[symbol(multiBuildClusterEnabled)]': undefined;
-    '[symbol(row data)]': undefined;
-    '[symbol(dirty variable)]': undefined;
-
-    // functions
-    getStageBuilds: () => unknown;
-    getBuilds: () => unknown;
-    getMetrics: () => unknown;
-};
 
 type Creator = {
     id: string;
@@ -461,36 +471,6 @@ type Meta = {
     };
 }
 
-// -----------------------------------------------------------------------------
-//      screwdriver-models.UserModel
-// -----------------------------------------------------------------------------
-/**
- * UserModel is defined in 'screwdriver-models/lib/user.js'
- *
- * @description import {UserModel} from 'screwdriver-models/lib/user'
- */
-export type UserModel = {
-    id: number;
-    username: string;
-    token: string;
-    scmContext: string;
-    settings: Record<string, settings>;
-}
 type settings = {
     showPRJobs: boolean;
 }
-
-// -----------------------------------------------------------------------------
-//      screwdriver-models.StageModel
-// -----------------------------------------------------------------------------
-/**
- * StageModel is defined in 'screwdriver-models/lib/stage.js'
- *
- * @description import {StageModel} from 'screwdriver-models/lib/stage'
- */
-export type StageModel = object;
-
-// -----------------------------------------------------------------------------
-//      Dependencies
-// -----------------------------------------------------------------------------
-
