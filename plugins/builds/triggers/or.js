f1ad444b21293f8dfe24ae364033e0f791028b10
Yuta Kaneda <yukaneda@yahoo-corp.jp>
2024-05-07 14:44:50 +0900

    refacotring trigger

--- plugins/builds/triggers/or.js
+++ plugins/builds/triggers/or.js
@@ -6,16 +6,13 @@ const { createInternalBuild, Status } = require('./helpers');
  * @typedef {import('screwdriver-models').BuildFactory} BuildFactory
  * @typedef {import('screwdriver-models').JobFactory} JobFactory
  * @typedef {import('screwdriver-models').PipelineFactory} PipelineFactory
- * @typedef {import('screwdriver-models/lib/pipeline').PipelineModel} PipelineModel
- * @typedef {import('screwdriver-models/lib/event').EventModel} EventModel
  * @typedef {import('screwdriver-models/lib/build').BuildModel} BuildModel
+ * @typedef {import('screwdriver-models/lib/event').EventModel} EventModel
  */
 /**
  * @property {BuildFactory} buildFactory
  * @property {JobFactory} jobFactory
  * @property {PipelineFactory} pipelineFactory
- * @property {PipelineModel} currentPipeline
- * @property {EventModel} currentEvent
  * @property {BuildModel} currentBuild
  * @property {number} username
  * @property {string} scmContext
@@ -23,17 +20,14 @@ const { createInternalBuild, Status } = require('./helpers');
 class OrTrigger {
     /**
      * Trigger the next jobs of the current job
-     * @param {import('../types/index').ServerApp} app                      Server app object
-     * @param {import('../types/index').ServerConfig} config              Configuration object
-     * @param {import('screwdriver-models/lib/event').EventModel} currentEvent
+     * @param {import('../types/index').ServerApp} app          Server app object
+     * @param {import('../types/index').ServerConfig} config    Configuration object
      */
-    constructor(app, config, currentEvent) {
+    constructor(app, config) {
         this.buildFactory = app.buildFactory;
         this.jobFactory = app.jobFactory;
         this.pipelineFactory = app.pipelineFactory;
 
-        this.currentPipeline = config.pipeline;
-        this.currentEvent = currentEvent;
         this.currentBuild = config.build;
         this.username = config.username;
         this.scmContext = config.scmContext;
@@ -41,15 +35,17 @@ class OrTrigger {
 
     /**
      * Trigger the next jobs of the current job
-     * @param {string} nextJobName                          Server app object
-     * @param {string} nextJobId                            Configuration object
+     * @param {EventModel} event
+     * @param {number} pipelineId
+     * @param {string} nextJobName
+     * @param {string} nextJobId
      * @param {Record<string, ParentBuild>} parentBuilds
      * @return {Promise<BuildModel|null>}
      */
-    async run(nextJobName, nextJobId, parentBuilds) {
+    async run(event, pipelineId, nextJobName, nextJobId, parentBuilds) {
         /** @type {BuildModel|null} */
         const nextBuild = await this.buildFactory.get({
-            eventId: this.currentEvent.id,
+            eventId: event.id,
             jobId: nextJobId
         });
 
@@ -67,14 +63,15 @@ class OrTrigger {
         return createInternalBuild({
             jobFactory: this.jobFactory,
             buildFactory: this.buildFactory,
-            pipelineId: this.currentPipeline.id,
+            pipelineId,
             jobName: nextJobName,
             username: this.username,
             scmContext: this.scmContext,
-            event: this.currentEvent, // this is the parentBuild for the next build
-            baseBranch: this.currentEvent.baseBranch || null,
+            event,
+            baseBranch: event.baseBranch || null,
             parentBuilds,
-            parentBuildId: this.currentBuild.id
+            parentBuildId: this.currentBuild.id,
+            start: true
         });
     }
 }
